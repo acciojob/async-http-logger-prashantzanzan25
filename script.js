f60c52c31661e3e27c20658d72fd1dbae7d71500
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-21 12:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const axios = require('axios');
+
+async function fetchData() {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+        console.log(response.data);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+fetchData();
+
+
+
+
